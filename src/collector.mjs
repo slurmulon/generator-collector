8ad90da027ea55572a49f96c45b0cc48691d5def
Erik Vavro <me@madhax.io>
2022-10-29 00:54:42 -0400

    :wrench: :bug: :checkered_flag: Introduced take and group queries in collector. Also various improvements and fixes in collector.

--- src/collector.mjs
+++ src/collector.mjs
@@ -10,10 +10,14 @@ import {
 import {
   find,
   filter,
+  map,
   yielding,
+  groupBy,
   wrapAsPromise
 } from 'js-coroutines'
 
+// TODO: Support `scope` or `limit` parameter for bounding results length to a moving bounded scope
+//  - Allows collection of infinite generators!
 export const collector = (it) => (...args) => {
   let results = []
   let current = null
@@ -21,7 +25,7 @@ export const collector = (it) => (...args) => {
   let done = false
 
   if (isAsyncGeneratorFunction(it)) {
-    throw TypeError(`collect cannot support async generator functions due to yield*`)
+    throw TypeError(`collector cannot support async generator functions due to yield*`)
   }
 
   if (!isGeneratorFunction(it)) {
@@ -33,9 +37,9 @@ export const collector = (it) => (...args) => {
     depth = 0
     done = false
 
-    const path = it(...args)
+    const iterator = it(...args)
 
-    for (const node of path) {
+    for (const node of iterator) {
       depth++
       current = node
       yield node
@@ -50,7 +54,12 @@ export const collector = (it) => (...args) => {
 
   const context = {
     find: wrapAsPromise(function* (selector = true, next = false) {
-      let node = gen.next()
+      // It the current iterated node exists, us it unless we're forcing an iteration
+      let node = current
+        ? next
+          ? gen.next()
+          : current
+        : gen.next()
 
       const known = yield* find(
         results,
@@ -62,8 +71,7 @@ export const collector = (it) => (...args) => {
         return yield entity(known, unwrap)
       }
 
-      // Continue iterating until we find, capture and return the first matching result.
-      // Yields control of the thread and then repeats this process upon subsequent queries.
+      // Continue iterating until we find, capture and return the first matching result
       while (!node?.done) {
         const value = yield entity(node.value, unwrap)
 
@@ -79,11 +87,11 @@ export const collector = (it) => (...args) => {
       return null
     }),
 
-    all: wrapAsPromise(function* (selector = true) {
+    all: wrapAsPromise(function* (selector = true, lazy = false) {
       // Flush the entire generator and capture all parsed results before filtering, allowing
       // user-provided selector functions (and matcher) to accept purely synchronous values.
       // In general we must iterate and parse the entire generator to know every matching result.
-      yield context.find(false, true)
+      if (!lazy) yield context.find(false, true)
 
       return yield* filter(
         results || [],
@@ -91,14 +99,51 @@ export const collector = (it) => (...args) => {
       )
     }),
 
-    last: wrapAsPromise(function* (selector = true) {
-      const matches = yield context.all(selector)
+    last: wrapAsPromise(function* (selector = true, lazy = false) {
+      const matches = yield context.all(selector, lazy)
 
       return matches[matches.length - 1] ?? null
     }),
 
-    // TODO: query: group(selector)
-    // TODO: query: take(count, selector)
+    group: wrapAsPromise(function* (selector = true, grouping, lazy = false) {
+      const matches = yield context.all(selector, lazy)
+
+      return yield* groupBy(
+        matches || [],
+        yielding(grouping)
+      )
+    }),
+
+    take: wrapAsPromise(function* (selector = true, count = 1, next = false) {
+      const items = []
+      let depth = 0
+
+      // Take from collected results when iteration is already complete
+      if (!next && done) {
+        const matches = yield *filter(
+          results,
+          yielding(matcher(selector))
+        )
+
+        return yield* map(
+          matches.slice(0, count),
+          yielding(value => entity(value, unwrap))
+        )
+      }
+
+      // Continue iterating and find the next matching results until we are done
+      while (depth < count && !done) {
+        const iterate = depth ? true : next
+        const item = yield context.find(selector, iterate)
+
+        if (item != null) {
+          items.push(item)
+          depth++
+        }
+      }
+
+      return items
+    }),
 
     clear () {
       gen.return(results)
