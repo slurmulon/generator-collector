72c211a6990f9c725f0eac2122d6239d6e25082d
Erik Vavro <me@madhax.io>
2022-11-14 23:59:12 -0500

    :vhs: :truck: :sparkles: Moved examples/each to its own folder

---
+++ examples/each/index.mjs
@@ -0,0 +1,42 @@
+import { collector } from '../../src/collector.mjs'
+import { each } from '../../src/each.mjs'
+
+const map = collector(each)
+
+// Create a flatMap version of `each` by simply wrapping our `map` collector with another `each` generator
+// Only handles 2d arrays, but could be refactored to be recursive
+const flat = (grid, resolver) => map(grid, function* (row) {
+  yield* each(row, resolver)
+})
+
+async function run () {
+  const table = [
+    [...Array(4)].map(() => Math.random() * 10000),
+    [...Array(4)].map(() => Math.random() * 10000),
+    [...Array(4)].map(() => Math.random() * 10000),
+    [...Array(4)].map(() => Math.random() * 10000),
+  ]
+
+  console.log('rounding and flattening...', table)
+
+  // Provide a plain resolver function that rounds each cell/value in a 2d matrix
+  const round = flat(table, cell => Math.round(cell))
+
+  // You can provide your resolver as a generator function, too
+  // const round = flat(table, function* (cell) {
+  //   console.log('das cell!', cell, Math.round(cell))
+
+  //   return yield Math.round(cell)
+  // })
+
+  // const all = await round.all()
+  const all = await round.take(6)
+
+  return all
+}
+
+run().then(result => {
+  console.log('iterated, rounded and flattened only 6 of 16 results:\n', result)
+
+  process.exit(0)
+})
