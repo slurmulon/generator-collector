de4c39013c86ce29a27242a38f6c75e5aa5b1bef
Erik Vavro <me@madhax.io>
2024-04-12 22:42:15 -0400

    :recycle: :vhs: :sparkles: More post-refactor cleanup to examples/each module

--- examples/each/index.mjs
+++ examples/each/index.mjs
@@ -1,63 +1,50 @@
 import { collector } from '../../src/collector.mjs'
 import { each } from '../../src/each.mjs'
 
-// Create a Collector generator from `each` (`each is a vanilla generator method)
-const map = collector(each)
-
-// Create a flatMap version of `each` by simply wrapping our `map` collector with another `each` generator
-const flat = (grid, resolver) => map(grid, function* (row) {
-  yield* each(row, resolver)
-})
+// Create a collector generator from `each` (by itself, `each` is a standard generator function)
+// This will behave as a recursive `flatMap` since every collected result is already stored to a flat array
+const flat = collector(each)
 
 async function run () {
-  // The standard synchronous approach... blocking, greedy and SLOW....
-  // const table = [
-  //   [...Array(4)].map(() => Math.random() * 10000),
-  //   [...Array(4)].map(() => Math.random() * 10000),
-  //   [...Array(4)].map(() => Math.random() * 10000),
-  //   [...Array(4)].map(() => Math.random() * 10000),
-  // ]
-
-  // The improved generator approach - lazy, scalable and efficient!
-  const table = function* (height = 100, width = 100, factor = 10000) {
-    let h = height,
-        w = width
-
-    while (h-- > 0) {
-      while (w-- > 0) {
-        yield Math.random() * factor
+  // Create our table generator, which lazily iterates through a 2d grid and yields a coordinate/id for each cell (<row>.<col>)
+  // e.g. ['1.1', '1.2', '1.3', '1.4', '2.1', '2.2', ...]
+  const table = function* (height = 1000, width = 4) {
+    for (let h = 1; h <= height; h++) {
+      for (let w = 1; w <= width; w++) {
+        yield `${h}.${w}`
       }
     }
   }
 
-  console.log('rounding and flattening...', table)
+  console.log('calculating and flattening...', table)
 
-  // Provide a plain resolver function that rounds each cell/value in a 2d matrix
+  // Provide a plain resolver function that rounds each cell/value in our table/grid/matrix
   // Either use the parameter defaults or provide your own to `table`
   // const round = flat(table(10, 10), cell => {
   const round = flat(table, cell => {
-    console.log('iterating and resolving cell', cell, Math.round(cell))
+    console.log('iterating and resolving cell', cell)
 
-    return Math.round(cell)
+    // Parse the returned cell value as a float on each iteration
+    return Number.parseFloat(cell)
   })
 
   // You can provide your resolver as a generator function, too!
   // const round = flat(table, function* (cell) {
-  //   return yield Math.round(cell)
+  //   return yield Number.parseFloat(cell)
   // })
 
-  const all = await round.take(6)
+  // Although our table contains 10000 cells by default, we only want the first 6
+  // Thanks to `take`, any cells beyond iteration 6 will never be calculated or visited!
+  const items = await round.take(6)
 
   return {
-    all,
+    items,
     total: round.state().depth
   }
 }
 
 run().then(result => {
-  // console.log('iterated, rounded and flattened only 6 of 16 results:\n', result)
-
-  console.log(`iterated, rounded and flattened only ${result.total} of 10000 potential results:\n`, result.all)
+  console.log(`iterated, calculated and flattened only ${result.total} of 4000 potential results:\n`, result.items)
 
   process.exit(0)
 })
